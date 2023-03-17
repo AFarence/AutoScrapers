6a2f05d21a6771690505629c81afe20fdeb34a19
AFarence <83982643+AFarence@users.noreply.github.com>
2023-03-17 11:35:07 -0400

    Combined .py files

---
+++ Redfin/redfin_county_scraper.py
@@ -0,0 +1,101 @@
+import pandas as pd
+import requests
+from bs4 import BeautifulSoup
+
+### For Google Sheets ###
+import os
+from oauth2client.service_account import ServiceAccountCredentials
+import gspread
+import json
+
+
+headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36"
+}
+
+scrape_dict = {"MiamiDade":"https://www.redfin.com/county/479/FL/Miami-Dade-County/filter/sort=hi-price,include=sold-1wk",
+               "PalmBeach":"https://www.redfin.com/county/486/FL/Palm-Beach-County/filter/sort=hi-price,include=sold-1wk",
+               "Broward":"https://www.redfin.com/county/442/FL/Broward-County/filter/sort=hi-price,include=sold-1wk",
+               "CookCounty":"https://www.redfin.com/county/727/IL/Cook-County/filter/sort=hi-price,include=sold-1wk"}
+
+scopes = [
+'https://www.googleapis.com/auth/spreadsheets',
+'https://www.googleapis.com/auth/drive'
+]
+
+def scrape_redfin(url,headers):
+
+    try:
+        response = requests.get(url,headers=headers)
+        soup = BeautifulSoup(response.content, 'html.parser')
+        try:
+            price_soup = soup.find_all('span',{'class':'homecardV2Price'})
+        except:
+            price_soup = None
+        try:
+            address_soup = soup.find_all('span',{'class':'collapsedAddress primaryLine'})
+        except:
+            address_soup = None
+        try:
+            card_soup = soup.find('div',{'class':'HomeCardsContainer flex flex-wrap'})
+            url_soup = card_soup.find_all('a')
+        except:
+            url_soup = None
+    except Exception as e:
+        print(f'{e} -- response fail!')
+        return None
+
+    # Get just the hyperlink
+    url_list = []
+    for x in url_soup:
+        url_list.append(x['href'])  
+
+    # Use a dictionary to keep track of elements we've already encountered
+    seen = {}
+
+    # Create a new list with duplicates removed
+    new_list = []
+    for item in url_list:
+        if item not in seen:
+            new_list.append(item)
+            seen[item] = True
+
+    data = {'Address':address_soup,'Price':price_soup,'URL':new_list}
+
+    df = pd.DataFrame(data)
+
+    df['URL'] = 'redfin.com' + df['URL'].astype(str)
+
+    for i, r in df.iterrows():
+        r['Price'] = r['Price'].text
+        r['Address'] = r['Address'].text
+
+    df['Price'] = df['Price'].str.replace('$','',regex=True)
+    df['Price'] = df['Price'].str.replace(',','',regex=True)
+    df['Price'] = pd.to_numeric(df['Price'])
+
+    df = df.sort_values(by='Price', ascending=False)
+
+    df.reset_index(inplace=True, drop=True)
+
+    return df
+
+def update_spreadsheet(spreadsheet, sheet_name, df):
+
+    credentials = ServiceAccountCredentials.from_json_keyfile_dict(
+    json.loads(os.environ.get('SERVICE_ACCOUNT_JSON')), scopes)
+    file = gspread.authorize(credentials)
+    sheet = file.open(spreadsheet).worksheet(sheet_name)
+
+    # Clear existing data
+    sheet.clear()
+
+    header = df.columns.tolist()
+    data = df.values.tolist()
+    sheet.insert_row(header, 1)
+    sheet.insert_data(data, 2)
+
+for key in scrape_dict.keys():
+    df = scrape_redfin(scrape_dict[key], headers)
+    update_spreadsheet(key, df)
+
