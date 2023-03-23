0e7c7d85b9a6c3507aa0861c3f1f5ae9d60210d4
AFarence <83982643+AFarence@users.noreply.github.com>
2023-03-23 00:34:27 -0400

    Update AgentScraper.py

--- Agent_Scraper/AgentScraper.py
+++ Agent_Scraper/AgentScraper.py
@@ -11,6 +11,8 @@ from bs4 import BeautifulSoup
 
 from time import sleep
 
+# Construct the full path to the CSV file
+csv_file_path = os.path.join(os.path.dirname(__file__))
 
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36"
